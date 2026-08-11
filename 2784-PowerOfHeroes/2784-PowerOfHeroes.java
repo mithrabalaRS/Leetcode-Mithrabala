ed0f2c249d6edce5ec32b57e66ed8e452b5ee11b
mithrabalaRS <mithrabalars@gmail.com>
2026-08-11 14:17:00 +0530

    Create 2784-PowerOfHeroes

---
+++ 2784-PowerOfHeroes/2784-PowerOfHeroes.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 2:17:01 PM
+class Solution {
+    public int sumOfPower(int[] nums) {
+        long res = 0, s = 0, base = 1000000007;
+        Arrays.sort(nums);
+        for (int x: nums) {
+            res = (res + (s + x) * x % base * x % base) % base;
+            s = (s * 2 + x) % base;
+        }
+        return (int)res;
+    }
+}
